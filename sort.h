0acb5b84f30f259e8b80e5784b74b8c39615b130
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-04-28 23:16:32 -0400

    task_2

--- sort.h
+++ sort.h
@@ -5,23 +5,26 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element
+* @next: Pointer to the next element
+*/
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
-/* Provided printing function */
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Your sorting function */
+/* Sorting algorithms */
+void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
